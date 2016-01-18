c9f915c67e9f76df4cd312526a34a6c12eace26b
George <george.bungarzescu@gmail.com>
2016-01-18 11:21:14 +0200

    Update Makefile.js

--- src/js_os/jsboot/Makefile.js
+++ src/js_os/jsboot/Makefile.js
@@ -1,1 +1,15 @@
+/*
+The code is used to create boot loader code from scratch. 
+It will run js code in order to build asm source code in one file in order to be compiled as a 
+boo
 
+Input code: js
+Intermediate code: asm and c code
+Depend on js-bootlib - the library for performing low level routines
+Output result: uefi/mbr boot code able to load ntldr, freebsd, linux and other os kernels
+
+Inspiration Sources: libreboot,coreboot,
+*/
+
+
+nodejs makeboot.js
